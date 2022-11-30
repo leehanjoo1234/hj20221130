c54864e0cc580e0c27fda631fc07d7ebd2cad692
82102 <82102@DESKTOP-LOCUK6S>
2022-11-30 21:58:50 +0900

    프로젝트 생성 환경구성

---
+++ hj20221130/src/test/java/com/oracle/hj20221130/Hj20221130ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.hj20221130;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hj20221130ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
